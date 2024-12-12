4c91c31d1551c887187c1595d4eaa985b206660b
nada <nadaelsharkawy545gmail.com>
2024-12-12 21:02:38 +0200

    testing user APIs final

--- routes/private/AdminAPI.js
+++ routes/private/AdminAPI.js
@@ -14,13 +14,24 @@ function handleAdminBackendApi(app) {
       });
     
       app.delete('/api/v1/users/:id', async (req, res)=> {
-    
+        const userId = req.params.id;
+
+        const trx = await db.transaction();
         try {
-          const query = `delete from "SEproject"."users" where user_id=${req.params.id}`;
-          const result = await db.raw(query);
-          if (result.rowCount === 0) {
+          await trx('SEproject.equipment_order')
+          .join('SEproject.orders', 'equipment_order.order_id', '=', 'orders.order_id')
+          .where('orders.user_id', userId)
+          .del();
+          await trx('SEproject.orders').where('user_id', userId).del();
+          await trx('SEproject.cart').where('user_id', userId).del();
+          await trx('SEproject.rating').where('user_id', userId).del();
+          const result = await trx('SEproject.users').where('user_id', userId).del();
+        
+          if (result === 0) {
+            await trx.rollback();
             return res.status(404).send("User not found");
         }
+        await trx.commit();
           return res.status(200).send("deleted succesfully");
         } catch (err) {
           console.log("eror message", err.message);
