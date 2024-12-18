258672dae6b4bf9bbae7b830c244c0bbb80ead39
nada <nadaelsharkawy545gmail.com>
2024-12-18 22:03:42 +0200

    f

---
+++ public/src/login.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+    $("#submit").click(function() {
+      const username = $('#username').val();
+      const password = $('#password').val();
+
+      const data = {
+        username,
+        password,
+      };
+
+      $.ajax({
+        type: "POST",
+        url: '/api/v1/user/login',
+        data  ,
+        success: function(serverResponse) {
+          if(serverResponse) {
+            alert("login successfully");
+            console.log("got to dashboard")
+            location.href = '/dashboard';
+          }
+        },
+        error: function(errorResponse) {
+          if(errorResponse) {
+            alert(`User login error: ${errorResponse.responseText}`);
+          }            
+        }
+      });
+    });
+  });
